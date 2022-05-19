41ede5d81fda9478c2bb88100174b31c8c779e14
JLit218 <jlit218@wgu.edu>
2022-05-19 17:34:55 -0400

    Finishing up BST and Hash Tables

---
+++ 06_Hash_Tables/00_Hash_Tables_Collisions.py
@@ -0,0 +1,6 @@
+# Deterministic/ only go one way.
+# List within a list
+# A collision is when you store a key value pair in the same index as another key value pair
+# a collision creates a list within a list or Separate Chaining
+# Linear probing - avoids collision/separate chaining by adding value pair in an empty index
+# Also known as Open Addressing
